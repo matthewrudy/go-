58bec5c4f4ce073930cb370e01ab8511b7d13c63
Matthew Rudy Jacobs <matthewrudyjacobs@gmail.com>
2018-01-29 00:35:06 +0000

    Use type aliases for each chinese type

--- 类型/类型.go
+++ 类型/类型.go
@@ -2,27 +2,27 @@
 package 类型
 
 // unsigned
-type T非整数8 uint8
-type T非整数16 uint16
-type T非整数32 uint32
-type T非整数64 uint64
+type T非整数8 = uint8
+type T非整数16 = uint16
+type T非整数32 = uint32
+type T非整数64 = uint64
 
 // signed
-type T整数8 int8
-type T整数16 int16
-type T整数32 int32
-type T整数64 int64
+type T整数8 = int8
+type T整数16 = int16
+type T整数32 = int32
+type T整数64 = int64
 
 // floats
-type T浮点數32 float32
-type T浮点數64 float64
+type T浮点數32 = float32
+type T浮点數64 = float64
 
 // pre-declared
-type T整数 int
-type T非整数 uint
+type T整数 = int
+type T非整数 = uint
 
 // string
-type T字符串 string
+type T字符串 = string
 
 // missing
 
